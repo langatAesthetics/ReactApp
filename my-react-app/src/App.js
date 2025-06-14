e03eabe6363bef3868b04a2f83802d616f44bff4
langatAesthetics <langataesthetics@gmail.com>
2025-06-14 12:07:39 +0300

    Update

---
+++ my-react-app/src/App.js
@@ -0,0 +1,116 @@
+
+import "./App.css";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import React, { useState } from 'react';
+
+
+
+function Header() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen)
+    };
+    return (
+        <header class="header">
+            <div class="logo">LA</div>
+            <div class="hamburger" onclick="toggleMenu()">
+                <i class="fa-solid fa-bars"></i>
+            </div>
+            <nav className={`navbar ${menuOpen ? 'show' : ''}`}>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Services</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+function Hero() {
+    return (
+        <section class="hero">
+            <div><img src="" alt="Image" /></div>
+            <div class="hero-content">
+                <h2>W3Lcome to mY Site3</h2>
+                <p>hchb hb bhbxhdcb</p>
+                <button class="btn">Book Now</button>
+            </div>
+        </section>
+    )
+}
+
+function About() {
+    return (
+        <section id="About">
+            <div>
+                <h2>About Me</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi autem odit enim veniam temporibus
+                    asperiores maxime et in. Unde, culpa. Laboriosam quam consequuntur perspiciatis corporis quia non alias
+                    voluptate aliquam.</p>
+            </div>
+        </section>
+    )
+}
+
+function Services() {
+    return (
+
+        <section id="Services">
+            <h2 style="text-align: center;">Services</h2>
+            <div class="card">
+                <h2>Website Development</h2>
+                <p>Interractive, TResponsive Websites</p>
+            </div>
+            <div class="card">
+                <h2>App Dev</h2>
+                <p>Interractive Mobile Apps</p>
+            </div>
+            <div class="card">
+                <h2>UI/UX Design</h2>
+                <p>Graphic design</p>
+            </div>
+        </section>
+    )
+}
+
+function Contact() {
+    return (
+        <section id="Contact">
+            <h2 style="text-align: center;">Contact</h2>
+            <div class="card">
+                <h2>Email<br /></h2>
+            </div>
+            <div class="card">
+                <h2>Mobile Phone</h2>
+            </div>
+        </section>
+    )
+}
+
+function Footer() {
+    return (
+        <footer class="footer">
+            <p>copyright 2025 | L Aesthetics</p>
+        </footer>
+    )
+}
+function App() {
+    return (
+        <>
+            <Header />
+            <Hero />
+            <About />
+            <Services />
+            <Contact />
+            <Footer />
+        </>
+    )
+}
+
+export default App;
+
+
+
